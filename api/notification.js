288c99b23c4f8874e996bc3fb454b7f26cb54c75
ibadnabihashmi <ibad.hashmi@yahoo.com>
2016-12-06 00:17:59 +0500

    frefer

--- api/notification.js
+++ api/notification.js
@@ -1,13 +1,46 @@
-var async       = require('async');
-var express     = require('express');
-var router      = express.Router();
-var fs          = require('fs');
-var User        = require('../models/User');
+var async           = require('async');
+var express         = require('express');
+var router          = express.Router();
+var fs              = require('fs');
+var User            = require('../models/User');
+var Preferences     = require('../models/Preferences');
+var Notification    = require('../models/Notification');
 
 router.post('/create', function (req,res) {
-  var tags = req.body.tags;
-  var fromUser = req.body.fromUser;
-  var catalogueTagged = req.body.catalogueTagged;
+  var tags = ['tag2','tag12'];
+  //var fromUser = req.body.fromUser;
+  //var catalogueTagged = req.body.catalogueTagged;
+  //var message = req.body.notificationMessage;
+  Preferences
+      .find({
+          tags:{
+              $in:tags
+          }
+      })
+      .select('userId')
+      .exec(function (err,prefs) {
+          async.eachSeries(prefs,function (pref,callback) {
+              var notification = new Notification({
+                  message: message,
+                  createdAt: Date.now(),
+                  userId: pref.userId,
+                  catalogue: catalogueTagged,
+              });
+              notification.save(function (err) {
+                  if(err){
+                      callback(err);
+                  }else{
+                      callback();
+                  }
+              });
+          },function (err) {
+              return res.status(200).send({
+                  status: 200,
+                  exception: null,
+                  message: 'Notifications sent'
+              });
+          })
+      });
 });
 
 router.get('/get',function (req,res) {
--- models/Notification.js
+++ models/Notification.js
@@ -1,11 +1,19 @@
 var mongoose    = require('mongoose');
 var User        = require('../models/User');
+var Catalogue   = require('../models/Catalogue');
 
 var notificationSchema = mongoose.Schema({
   userId:{
     type:mongoose.Schema.Types.ObjectId,
     ref:'User'
-  }
+  },
+  catalogue:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Catalogue'
+  },
+  message: String,
+  createdAt: Date,
+  read: false
 });
 var Notification = mongoose.model('Notification',notificationSchema);
 module.exports = Notification;
